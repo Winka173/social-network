cb9dfac55b5af86ae01561f6577657dd2a6f3094
DESKTOP-BUTAKGM\Phu Quoc <chippy1731998@gmail.com>
2021-10-05 02:25:48 +0700

    Add firebase cloud message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
+import React, { useEffect } from "react";
 import { Home, Login, Messenger } from "./Pages/index";
 import { AuthProvider } from "./Store/AuthContext";
 import { Provider } from "react-redux";
 import store from "./Store/Redux";
+import { getToken } from "./Firebase/Firebase";
 
 function App() {
+  useEffect(() => {
+    getToken().then((data) => console.log(data));
+  }, []);
+
   return (
     <BrowserRouter>
       <Provider store={store}>
--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import "firebase/messaging";
 
 const initData = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -10,6 +11,7 @@ const initData = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = !firebase.apps.length
@@ -19,5 +21,17 @@ const firebaseApp = !firebase.apps.length
 const auth = firebaseApp.auth();
 const db = firebaseApp.firestore();
 const storage = firebaseApp.storage().ref();
+const messaging = firebase.messaging();
 
-export { auth, db, storage };
+const getToken = async () => {
+  try {
+    let currentToken = await messaging.getToken({
+      vapidKey: process.env.REACT_APP_FIREBASE_WEB_PUSH_CERTIFICATES,
+    });
+    return currentToken;
+  } catch (error) {
+    console.log("Token error: ", error);
+  }
+};
+
+export { auth, db, storage, messaging, getToken };
