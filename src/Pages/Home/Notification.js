2f80a73232585ab774ae2b44343f98073ccd2924
DESKTOP-BUTAKGM\Phu Quoc <chippy1731998@gmail.com>
2021-10-06 14:50:01 +0700

    Fix notification animation duration

--- src/Pages/Home/Notification.js
+++ src/Pages/Home/Notification.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import styles from "./Notification.module.css";
 import { CloseOutlined } from "@ant-design/icons";
 import { useSelector, useDispatch } from "react-redux";
-import { useAuthContext } from "../../Store/AuthContext";
 import reactDom from "react-dom";
 
 let init = true;
@@ -11,29 +10,26 @@ const NotificationOverlay = () => {
   const [show, setShow] = useState(false);
   const notification = useSelector((state) => state.notification.notification);
   const dispatch = useDispatch();
-  const { user } = useAuthContext();
 
   useEffect(() => {
     if (init) {
       init = false;
       return;
     }
-    let timer;
     if (notification) {
       setShow(true);
-
-      timer = setTimeout(() => {
-        setShow(false);
-      }, 4800);
     }
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [notification, dispatch, user.uid]);
+  }, [notification, dispatch]);
+
+  const onAnimationEnd = (event) => {
+    if (event.animationName.includes("fadeout")) {
+      setShow(false);
+    }
+  };
 
   if (show) {
     return (
-      <div className={styles.notification}>
+      <div onAnimationEnd={onAnimationEnd} className={styles.notification}>
         <div className={styles.title}>
           <span>New Notification</span>
           <button onClick={() => setShow(false)}>
